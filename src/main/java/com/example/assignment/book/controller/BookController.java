7a7ffb7feecfef0ae49b345ff75f2f53fa9e29ea
berry <kioho0213@gmail.com>
2024-02-22 01:42:25 +0900

    [ add ] - [ 스웨거 추가 ]
    [
      스웨거 관련 태그 추가
    ]

--- src/main/java/com/example/assignment/book/controller/BookController.java
+++ src/main/java/com/example/assignment/book/controller/BookController.java
@@ -2,6 +2,9 @@ package com.example.assignment.book.controller;
 
 import com.example.assignment.book.controller.request.BookRequest;
 import com.example.assignment.book.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -12,12 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "도서 관련")
 @RequestMapping("/api/books")
 public class BookController {
 
     private final BookService service;
 
     @PostMapping
+    @Operation(summary = "도서 위탁", description = "위탁 도서 정보를 등록하는 API 입니다.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "정상적으로 위탁 도서정보가 등록되었습니다.")
+            })
     public ResponseEntity<Void> consignment(@Validated @RequestBody BookRequest request) {
         service.consignment(request);
 
--- src/main/java/com/example/assignment/book/controller/request/BookRequest.java
+++ src/main/java/com/example/assignment/book/controller/request/BookRequest.java
@@ -1,20 +1,25 @@
 package com.example.assignment.book.controller.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.hibernate.validator.constraints.ISBN;
 
+@Schema(description = "도서 위탁 요청 객체")
 public record BookRequest(
 
 
+        @Schema(description = "도서 명", example = "컬처, 문화로 쓴 세계사")
         @NotBlank(message = "도서 명은 필수입니다.")
         String title,
 
+        @Schema(description = "ISBN", example = "9791167741370")
         @ISBN(type = ISBN.Type.ANY, message = "ISBN 형식이 아닙니다.")
         @NotBlank(message = "ISBN는 필수입니다.")
         String isbn,
 
+        @Schema(description = "대여 가격", example = "1500")
         @Positive(message = "대여 금액은 0보다 커야합니다.")
         @NotNull(message = "대여 금액은 필수입니다.")
         Integer rentalPrice
