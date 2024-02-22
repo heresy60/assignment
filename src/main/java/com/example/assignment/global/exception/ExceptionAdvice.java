5dc3e25961bf22264426955b29495924ec9837cd
berry <kioho0213@gmail.com>
2024-02-22 17:17:23 +0900

    [ add ] - [ 에러 처리 추가 ]
    [
      요구한 자료형이 아닐 경우에 대한 에러 처리 추가
    ]

--- src/main/java/com/example/assignment/global/exception/ExceptionAdvice.java
+++ src/main/java/com/example/assignment/global/exception/ExceptionAdvice.java
@@ -2,18 +2,38 @@ package com.example.assignment.global.exception;
 
 import com.example.assignment.global.exception.response.BaseErrorResponse;
 import com.example.assignment.global.exception.response.MethodArgumentsValidErrorResponse;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Objects;
+
 @Slf4j
 @RestControllerAdvice
 public class ExceptionAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public BaseErrorResponse httpMessageNotReadableException(HttpMessageNotReadableException e) {
+
+
+        if (e.getCause() instanceof InvalidFormatException cause) {
+
+            String fieldName = cause.getPath().get(0).getFieldName();
+            return new BaseErrorResponse( fieldName + " 필드 값이 잘못 된 자료형입니다.");
+        } else {
+
+            log.error(e.getMessage());
+            return new BaseErrorResponse("서버에서 처리 중 에러가 발생했습니다.");
+        }
+    }
 
     /**
      * MethodArgumentNotValidException 에러 처리하기 위한 메소드
@@ -29,7 +49,11 @@ public class ExceptionAdvice {
                 "요청 값을 검증 중 오류가 발생했습니다.",
                 errors.getFieldErrors().stream()
                         .map(
-                                fieldError -> new MethodArgumentsValidErrorResponse.DetailMessage(fieldError.getDefaultMessage(), fieldError.getField())
+                                fieldError -> {
+
+                                    String message = Objects.requireNonNull(fieldError.getCode()).contentEquals("typeMismatch") ? "잘못 된 자료형입니다." : fieldError.getDefaultMessage();
+                                    return new MethodArgumentsValidErrorResponse.DetailMessage(message, fieldError.getField());
+                                }
                         ).toList());
     }
     
