0682f2136df25262edf9c2f39bcf853ea3c84f72
berry <kioho0213@gmail.com>
2024-02-26 19:18:12 +0900

    [ fix ] - [ 정렬 관련 수정 ]
    [
      대여 수 관련 정렬 조건 수정
    ]

--- src/main/java/com/example/assignment/book/enums/BookSortProperty.java
+++ src/main/java/com/example/assignment/book/enums/BookSortProperty.java
@@ -3,7 +3,7 @@ package com.example.assignment.book.enums;
 import com.example.assignment.global.controller.request.SortingProperty;
 
 public enum BookSortProperty implements SortingProperty {
-    createDate("id"), price("rentalPrice"), rentalCount("rentalCount");
+    createDate("id"), price("rentalPrice"), rentalCount("rentalInfo.rentalCount");
 
     private final String column;
 
